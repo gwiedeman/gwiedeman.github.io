9747dbf8fa1bfc67fce53344369fd9bce90978ca
gwiedeman <gwiedeman@albany.edu>
2016-11-19 17:28:40 -0500

    fixed js issue

--- js/main.js
+++ js/main.js
@@ -43,6 +43,14 @@ if(!tap){
 			$("#abs5").hide()
 			$(".abstract-default").show()
 		}); 
+		$('#writing-6').mouseleave(function () {
+			$("#abs6").hide()
+			$(".abstract-default").show()
+		}); 
+		$('#writing-7').mouseleave(function () {
+			$("#abs7").hide()
+			$(".abstract-default").show()
+		}); 
 	}
 
 }
